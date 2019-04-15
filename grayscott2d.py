34a092fdf85c72172c1d4bc6d29aa320ca00f622
sanketyd <yadavsanket17@yahoo.in>
2019-04-15 20:42:19 +0530

    Added overlap initialization

--- grayscott2d.py
+++ grayscott2d.py
@@ -4,12 +4,12 @@ import os
 from tqdm import tqdm
 
 
-def grid_gen(grid_size=256, u_per=0.5, v_per=0.25, per_size=20):
+def grid_gen(grid_size=256, u_per=0.5, v_per=0.25, per_size=20, per_type='default', **kwargs):
 
     grid_u = np.ones((grid_size, grid_size))
     grid_v = np.zeros((grid_size, grid_size))
 
-    x_start, y_start = int(grid_size/2 - 10), int(grid_size/2 - 10)
+    x_start, y_start = int(grid_size/2 - per_size/2), int(grid_size/2 - per_size/2)
     x_end, y_end = x_start + per_size, y_start + per_size
 
     for i in range(x_start, x_end):
@@ -22,6 +22,21 @@ def grid_gen(grid_size=256, u_per=0.5, v_per=0.25, per_size=20):
 
             grid_u[i, j] *= perturb[np.random.randint(0, 2)]
             grid_v[i, j] *= perturb[np.random.randint(0, 2)]
+
+    if per_type == 'overlap':
+        x_start, y_start = int((x_start+x_end)/2), int((y_start+y_end)/2)
+        x_end, y_end = int(x_start + per_size/2), int(y_start + per_size/2)
+
+        for i in range(x_start, x_end):
+            for j in range(y_start, y_end):
+
+                grid_u[i, j] = kwargs['u_per_new']
+                grid_v[i, j] = kwargs['v_per_new']
+
+                perturb = [0.99, 1.01]
+
+                grid_u[i, j] *= perturb[np.random.randint(0, 2)]
+                grid_v[i, j] *= perturb[np.random.randint(0, 2)]
 
     return grid_u, grid_v
 
@@ -36,8 +51,8 @@ def double_derivative_2d(grid, h):
 
 def finite_diff(grid_u, grid_v, h, timesteps, dt, f, k, D_u, D_v, frame_rate=100):
     fig, ax = plt.subplots()
-    ax.axes.get_yaxis().set_visible(False)
-    ax.axes.get_xaxis().set_visible(False)
+    # ax.axes.get_yaxis().set_visible(False)
+    # ax.axes.get_xaxis().set_visible(False)
     im = ax.imshow(grid_u, interpolation='nearest')
     fig.colorbar(im)
 
@@ -58,6 +73,7 @@ def finite_diff(grid_u, grid_v, h, timesteps, dt, f, k, D_u, D_v, frame_rate=100
             counter += 1
 
     im.set_data(grid_u)
+    plt.title(r'f={}, k={}, $\Delta$t={}, $\Delta$x={}, $D_u$={}, $D_v$={}'.format(f, k, dt, h, D_u, D_v))
     fig.savefig('res_{}_{}.png'.format(f, k))
 
 
@@ -69,7 +85,9 @@ def make_animation(f, k):
 
 
 if __name__ == "__main__":
-    grid_u, grid_v = grid_gen()
-    f, k = 0.025, 0.05
-    finite_diff(grid_u, grid_v, 0.01, 200000, 0.05, f, k, 0.00002, 0.00001)
+    kwargs = {'u_per_new': 0.2, 'v_per_new': 0.6}
+    grid_u, grid_v = grid_gen(u_per=0.6, v_per=0.6, per_size=10, per_type='overlap', **kwargs)
+    # grid_u, grid_v = grid_gen()
+    f, k = 0.062, 0.065
+    finite_diff(grid_u, grid_v, 0.01, 100000, 0.5, f, k, 0.00002, 0.00001)
     make_animation(f, k)
